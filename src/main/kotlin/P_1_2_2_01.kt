6dd1eb44db984f2b0628763e93341913b7214ec3
Torb Lunde Miranda <1179046+torb-xyz@users.noreply.github.com>
2021-05-22 09:28:44 +0200

    Commented version of byte buffer

--- src/main/kotlin/P_1_2_2_01.kt
+++ src/main/kotlin/P_1_2_2_01.kt
@@ -5,6 +5,12 @@ import org.openrndr.extensions.Screenshots
 import org.openrndr.math.Vector2
 import utils.QuitOnEsc
 import java.lang.Double.max
+import java.nio.ByteBuffer
+
+interface PixelInterface {
+    fun makeReady()
+    operator fun get(x: Int, y: Int): ColorRGBa
+}
 
 fun main() = application {
     configure {
@@ -17,8 +23,11 @@ fun main() = application {
         extend(Screenshots())
 
 
-        fun sizedPixelData(image: ColorBuffer): ColorBufferShadow {
-            val rtImage = renderTarget(width, height) { colorBuffer(type = ColorType.UINT8) }
+        fun sizedPixelData(image: ColorBuffer): PixelInterface {
+            val rtImage = renderTarget(width, height) { colorBuffer(
+                format = ColorFormat.RGB,
+                type = ColorType.FLOAT32
+            ) }
 
             drawer.withTarget(rtImage) {
                 drawer.image(image, Vector2.ZERO, drawer.bounds.width, drawer.bounds.height)
@@ -27,14 +36,48 @@ fun main() = application {
             val shadow = rtImage.colorBuffer(0).shadow
             shadow.download()
 
-            return shadow
+            return object : PixelInterface {
+                override fun makeReady() {}
+                override operator fun get(x: Int, y: Int) = shadow[x, y]
+            }
         }
 
 //        var colorComparator: Comparator<ColorRGBa> = Comparator { a, b ->
 //            a.luminance.compareTo(b.luminance)
 //        }
 
-
+        // Potentially higher performance but would never figure out…
+//        fun sizedPixelData(image: ColorBuffer): PixelInterface {
+//            val rtImage = renderTarget(width, height) { colorBuffer(
+//                format = ColorFormat.RGB,
+//                type = ColorType.FLOAT32
+//            ) }
+//
+//            drawer.withTarget(rtImage) {
+//                drawer.image(image, Vector2.ZERO, drawer.bounds.width, drawer.bounds.height)
+//            }
+//
+//            val cb = rtImage.colorBuffer(0)
+//            val bfMultiplier = cb.format.componentCount * cb.type.componentSize
+//            val capacity = cb.width * cb.height * bfMultiplier
+//            val bytes = ByteBuffer.allocateDirect(capacity)
+//            cb.read(bytes)
+//            val doubles = bytes.asDoubleBuffer()
+//
+//            return object : PixelInterface {
+//                override fun makeReady() {
+//                    bytes.rewind()
+//                }
+//
+//                override fun get(x: Int, y: Int): ColorRGBa {
+//                    val i = x * y
+//                    val r = doubles[i]
+//                    val g = doubles[i+1]
+//                    val b = doubles[i+3]
+//                    return ColorRGBa(r,g,b)
+//                }
+//            }
+//        }
 
         // Used to access pixel data
         var image = loadImage("data/images/cheeta.jpg")
@@ -54,15 +97,16 @@ fun main() = application {
             val rectSize = max(mouse.position.x, 10.0)
             val rectSizeI = rectSize.toInt()
 
-            for (x in 0 until width step rectSizeI) {
-                for (y in 0 until height step rectSizeI) {
+            imageData.makeReady()
+            for (y in 0 until height step rectSizeI) {
+                for (x in 0 until width step rectSizeI) {
                     colors.add(imageData[x, y])
                 }
             }
 
-            colors.sortWith { a, b ->
-                a.luminance.compareTo(b.luminance)
-            }
+//            colors.sortWith { a, b ->
+//                a.luminance.compareTo(b.luminance)
+//            }
             // TODO: sort the colors
 
             drawer.stroke = null
@@ -77,7 +121,7 @@ fun main() = application {
                 }
 
                 drawer.fill = it
-                drawer.rectangle(y, x, rectSize, rectSize)
+                drawer.rectangle(x, y, rectSize, rectSize)
             }
         }
     }
