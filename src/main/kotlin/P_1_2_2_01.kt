5cb0bdabf40f23df19d288e7c6e9ae17b475525a
Torb Lunde Miranda <1179046+torb-xyz@users.noreply.github.com>
2021-05-22 09:29:18 +0200

    Back to only shadow

--- src/main/kotlin/P_1_2_2_01.kt
+++ src/main/kotlin/P_1_2_2_01.kt
@@ -5,12 +5,7 @@ import org.openrndr.extensions.Screenshots
 import org.openrndr.math.Vector2
 import utils.QuitOnEsc
 import java.lang.Double.max
-import java.nio.ByteBuffer
 
-interface PixelInterface {
-    fun makeReady()
-    operator fun get(x: Int, y: Int): ColorRGBa
-}
 
 fun main() = application {
     configure {
@@ -23,7 +18,7 @@ fun main() = application {
         extend(Screenshots())
 
 
-        fun sizedPixelData(image: ColorBuffer): PixelInterface {
+        fun sizedPixelData(image: ColorBuffer): ColorBufferShadow {
             val rtImage = renderTarget(width, height) { colorBuffer(
                 format = ColorFormat.RGB,
                 type = ColorType.FLOAT32
@@ -36,48 +31,13 @@ fun main() = application {
             val shadow = rtImage.colorBuffer(0).shadow
             shadow.download()
 
-            return object : PixelInterface {
-                override fun makeReady() {}
-                override operator fun get(x: Int, y: Int) = shadow[x, y]
-            }
+            return shadow
         }
 
 //        var colorComparator: Comparator<ColorRGBa> = Comparator { a, b ->
 //            a.luminance.compareTo(b.luminance)
 //        }
 
-        // Potentially higher performance but would never figure out…
-//        fun sizedPixelData(image: ColorBuffer): PixelInterface {
-//            val rtImage = renderTarget(width, height) { colorBuffer(
-//                format = ColorFormat.RGB,
-//                type = ColorType.FLOAT32
-//            ) }
-//
-//            drawer.withTarget(rtImage) {
-//                drawer.image(image, Vector2.ZERO, drawer.bounds.width, drawer.bounds.height)
-//            }
-//
-//            val cb = rtImage.colorBuffer(0)
-//            val bfMultiplier = cb.format.componentCount * cb.type.componentSize
-//            val capacity = cb.width * cb.height * bfMultiplier
-//            val bytes = ByteBuffer.allocateDirect(capacity)
-//            cb.read(bytes)
-//            val doubles = bytes.asDoubleBuffer()
-//
-//            return object : PixelInterface {
-//                override fun makeReady() {
-//                    bytes.rewind()
-//                }
-//
-//                override fun get(x: Int, y: Int): ColorRGBa {
-//                    val i = x * y
-//                    val r = doubles[i]
-//                    val g = doubles[i+1]
-//                    val b = doubles[i+3]
-//                    return ColorRGBa(r,g,b)
-//                }
-//            }
-//        }
 
         // Used to access pixel data
         var image = loadImage("data/images/cheeta.jpg")
@@ -97,7 +57,6 @@ fun main() = application {
             val rectSize = max(mouse.position.x, 10.0)
             val rectSizeI = rectSize.toInt()
 
-            imageData.makeReady()
             for (y in 0 until height step rectSizeI) {
                 for (x in 0 until width step rectSizeI) {
                     colors.add(imageData[x, y])
